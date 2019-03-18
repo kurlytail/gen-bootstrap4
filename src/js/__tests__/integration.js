f451f5ec9e24c0db886febd4d0c488c9557fc467
ashwin <ashwinkn@hotmail.com>
2019-03-17 21:41:17 -0500

    generated by     - gen-lib@1.0.8     - @kurlytail/gen-sgen@1.0.11

---
+++ src/js/__tests__/integration.js
@@ -0,0 +1,26 @@
+import { execSync } from 'child_process';
+
+describe('# integration test', () => {
+    beforeEach(() => {
+        execSync('rm -rf testoutput');
+    });
+
+    it('## should print help options', () => {
+        let output = execSync('npm run build').toString();
+        output = execSync('sgen -g `pwd`/dist/bootstrap4.min.js -h').toString();
+        expect(output).toMatchSnapshot();
+    });
+
+    it('## should generate design and run bootstrap4 commands', () => {
+        let output = execSync('npm run build').toString();
+        output = execSync(
+            'sgen -g `pwd`/dist/bootstrap4.min.js -d src/test/fixture/design.json -o testoutput'
+        ).toString();
+        output = output.replace(/info: Loaded generator .*bootstrap4.min.js.*/, '');
+        expect(output).toMatchSnapshot();
+        output = execSync('npm install', { cwd: 'testoutput' }).toString();
+        output = execSync('npm run lint', { cwd: 'testoutput' }).toString();
+        output = execSync('npm run build', { cwd: 'testoutput' }).toString();
+        output = execSync('npm run gen', { cwd: 'testoutput' }).toString();
+    });
+});
